49926c7a039f2bfadc840418b1be45b6c7a74a0d
justHman <hmantools11@gmail.com>
2025-10-30 19:31:15 +0700

    bert-base-multilingual-uncased

--- models/blip.py
+++ models/blip.py
@@ -136,7 +136,7 @@ class BLIP_Decoder(nn.Module):
         
         prompt = [self.prompt] * image.size(0)
         input_ids = self.tokenizer(prompt, return_tensors="pt").input_ids.to(image.device) 
-        input_ids[:,0] = self.tokenizer.bos_token_id
+        input_ids[:, 0] = self.tokenizer.bos_token_id
         input_ids = input_ids[:, :-1] 
 
         if sample:
@@ -185,7 +185,8 @@ def blip_feature_extractor(pretrained='',**kwargs):
 
 def init_tokenizer():
     # tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    tokenizer = AutoTokenizer.from_pretrained('vinai/phobert-base')
+    # tokenizer = AutoTokenizer.from_pretrained('vinai/phobert-base')
+    tokenizer = AutoTokenizer.from_pretrained('bert-base-multilingual-uncased')
     tokenizer.add_special_tokens({'bos_token':'[DEC]'})
     tokenizer.add_special_tokens({'additional_special_tokens':['[ENC]']})       
     tokenizer.enc_token_id = tokenizer.additional_special_tokens_ids[0]  
