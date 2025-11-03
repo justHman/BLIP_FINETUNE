59eb349d8310542f32ad39045bd2fec4737793f2
justHman <hmantools11@gmail.com>
2025-11-03 12:20:14 +0700

    fix path best cktpoint

--- train_caption.py
+++ train_caption.py
@@ -195,7 +195,7 @@ def main(args, config):
                 if score > best:
                     best = score
                     best_epoch = epoch     
-                    model_path = os.path.join(args.output_dir, 'best_checkpoint_epoch_%d.pth'%epoch)     
+                    model_path = os.path.join(args.output_dir, 'best_checkpoint.pth')     
                     print(f"==> Best checkpoint saved at: {model_path}")
                     print(f"Epoch: {best_epoch} - CIDEr: {val['CIDEr']:.2f} + Bleu_4: {val['Bleu_4']:.2f} = {best:.2f}")      
                     torch.save(save_obj, model_path) 
